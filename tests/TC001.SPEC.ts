437d9fc381de34743fccdff60b4f0534b4ba8db0
ds2income <ds2income@gmail.com>
2026-08-08 08:41:30 +0530

    Initial Playwright project2

--- tests/TC001.SPEC.ts
+++ tests/TC001.SPEC.ts
@@ -3,4 +3,5 @@ test('First Tc001',async({page})=>{
     await page.goto("https://google.com");
     await page.waitForTimeout(3000);
     console.log("app opened")
+    console.log("Pushed in github")
 })
